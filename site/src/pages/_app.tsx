2b4382e1b857b4c5b110ccfeac8f0848d2908136
herbertdesousa <sousaherbert138@gmail.com>
2022-03-19 16:14:57 -0300

    fix: eslint line

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -11,7 +11,10 @@ const App = ({ Component, pageProps }: any) => (
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Poppins:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
     </Head>
 
     <Component {...pageProps} />
