bf0c1453cfa406d3729e1f22852468f3f73184d8
herbertdesousa <sousaherbert138@gmail.com>
2023-11-15 11:14:39 -0300

    SETUP: firebase auth

--- mobile-v2/app/src/main/java/com/example/project_air/MainActivity.kt
+++ mobile-v2/app/src/main/java/com/example/project_air/MainActivity.kt
@@ -3,41 +3,121 @@ package com.example.project_air
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.project_air.ui.theme.ProjectairTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class SignUpEmailInUseException() : Exception()
+class SignInInvalidCredentialsException() : Exception()
 
 class MainActivity : ComponentActivity() {
+  private var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+  suspend fun signUp(email: String, password: String): FirebaseUser? {
+    return try {
+      val result = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+
+      result.user
+    } catch (e: FirebaseAuthUserCollisionException) {
+      throw SignUpEmailInUseException()
+    } catch (e: Exception) {
+      e.printStackTrace()
+      null
+    }
+  }
+
+  suspend fun signIn(email: String, password: String): FirebaseUser? {
+    return try {
+      val result = firebaseAuth.signInWithEmailAndPassword(email, password).await()
+
+      result.user
+    } catch (e: FirebaseException) {
+      throw SignInInvalidCredentialsException()
+    } catch (e: Exception) {
+      println(e)
+      // Handle other exceptions here
+      null
+    }
+  }
+
+  fun signOut() {
+    firebaseAuth.signOut()
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      ProjectairTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+      var currentUser by remember { mutableStateOf(firebaseAuth.currentUser) }
+
+      var errors by remember { mutableStateOf("") }
+
+      Column {
+        if (currentUser != null) {
+          Text(text = "UID: ${currentUser!!.uid}")
+          Text(text = "email: ${currentUser!!.email}")
+          Button(onClick = {
+            signOut()
+            currentUser = null
+          }) {
+            Text(text = "Sign Out")
+          }
+        }
+
+        if (currentUser == null) {
+          Button(onClick = {
+
+            lifecycleScope.launch {
+              try {
+                errors = ""
+
+                val user = signUp("hbt@email.com", "123456")
+
+                if (user !== null) currentUser = user
+              } catch (e: SignUpEmailInUseException) {
+                errors = "Email already in use"
+              }
+            }
+          }) {
+            Text(text = "sign up")
+          }
+
+          Button(onClick = {
+            lifecycleScope.launch {
+              try {
+                errors = ""
+
+                val (email, password) = when ((0..1).random()) {
+                  1 -> listOf("hbt@email.com", "123456")
+                  else -> listOf("wrong@email.com", "786545")
+                }
+
+                println("$email and $password")
+
+                val user = signIn(email, password)
+
+                if (user !== null) currentUser = user
+              } catch (e: SignInInvalidCredentialsException) {
+                errors = "Invalid Email/Password"
+              }
+            }
+          }) {
+            Text(text = "sign in")
+          }
+
+          Text(text = errors)
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  ProjectairTheme {
-    Greeting("Android")
   }
 }
