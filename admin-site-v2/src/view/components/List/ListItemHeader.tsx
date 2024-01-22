749345354618c243cb6081b0317086daab1a94df
herbertdesousa <sousaherbert138@gmail.com>
2024-01-22 20:54:33 -0300

    FIX: list item header items alignment

--- admin-site-v2/src/view/components/List/ListItemHeader.tsx
+++ admin-site-v2/src/view/components/List/ListItemHeader.tsx
@@ -4,7 +4,7 @@ type Props = { title: string; subTitle: string };
 
 export function ListItemHeader({ title, subTitle }: Props) {
   return (
-    <div className="justify-between self-stretch flex gap-5 py-0.5 w-full">
+    <div className="justify-between items-center self-stretch flex gap-5 py-0.5 w-full">
       <div className="flex flex-col overflow-hidden">
         <strong className="text-slate-900 text-lg font-medium grow whitespace-nowrap text-ellipsis overflow-hidden">
           {title}
