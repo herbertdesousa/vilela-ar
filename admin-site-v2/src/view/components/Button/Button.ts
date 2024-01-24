45cf87535a5073a5e1a91d6fd806c3df8ed79b69
herbertdesousa <sousaherbert138@gmail.com>
2024-01-24 19:23:51 -0300

    FEAT: Button component

---
+++ admin-site-v2/src/view/components/Button/Button.ts
@@ -0,0 +1,3 @@
+export { ButtonIcon as Icon } from './ButtonIcon';
+export { ButtonRoot as Root } from './ButtonRoot';
+export { ButtonText as Text } from './ButtonText';
--- admin-site-v2/src/view/components/index.ts
+++ admin-site-v2/src/view/components/index.ts
@@ -1,2 +1,3 @@
+export * as Button from './Button/Button';
 export * as List from './List/List';
 export { StepIndicator } from './StepIndicator';
